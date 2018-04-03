1a701182476e633e0129c3b035aa46d3762fae96
mikeb <MikeB@magenic.com>
2018-04-03 13:39:31 -0400

    first

---
+++ src/foo4A.ps1
@@ -0,0 +1,12 @@
+﻿ Param (
+    [parameter(mandatory)]
+    [String]$vmName
+ )
+
+#$vmName = 'EYACA-DShare'
+
+$Text = get-content c:/script/test.txt
+$Text = $Text -replace "robin", $vmName
+Write-Output $Text
+set-content c:/script/test.txt $Text
+Write-Output "complete"
